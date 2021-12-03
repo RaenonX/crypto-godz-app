62903b3201063e07e8f4e8a080a1af2228b1a3e5
RaenonX <raenonx0710@gmail.com>
2021-12-03 03:31:48 -0600

    Removed unused code

--- src/types/battleOptions.ts
+++
@@ -1,7 +0,0 @@
-import {RewardData} from '../data/rewards';
-
-
-export type BattleOption = {
-  reward: RewardData,
-  expectedReward: number,
-};
--- src/utils/iter.ts
+++ src/utils/iter.ts
@@ -1,57 +1,6 @@
 import {sumAccumulator} from './accumulator';
 
 
-export function* product<T, R>(
-  sets: T[],
-  getElemMaxIdx: (elem: T) => number,
-  transformElement: (item: T, counter: number) => R,
-  isValid?: (combination: R[]) => boolean,
-): Generator<R[]> {
-  const setLength = sets.length;
-  const elemMaxIdx = [...Array(setLength).keys()].map((idx) => getElemMaxIdx(sets[idx]));
-
-  const counterToReturn = (counter: number[]): R[] => {
-    return counter.map((count, index) => transformElement(sets[index], count));
-  };
-
-  const counterToCurrentIndex = (counter: number[]): number => {
-    return counter
-      .map((count, index) => (
-        count * (
-          elemMaxIdx
-            .slice(index + 1)
-            .reduce((prev, curr) => prev * curr, 1)
-        )
-      ))
-      .reduce((prev, curr) => prev + curr);
-  };
-
-  const possibilities = sets.map((setElem) => getElemMaxIdx(setElem)).reduce((prev, curr) => prev * curr);
-
-  function* helper(arrCounter: number[], setIdx: number): Generator<R[]> {
-    if (++setIdx >= setLength) {
-      const ret = counterToReturn(arrCounter);
-
-      const curIdx = counterToCurrentIndex(arrCounter);
-      if (curIdx % 100000 === 0) {
-        console.log(`${curIdx} / ${possibilities} (${(curIdx / possibilities * 100).toFixed(2)}%)`);
-      }
-
-      if (isValid && !isValid(ret)) {
-        return;
-      }
-
-      yield ret;
-    } else {
-      for (let i = 0; i < elemMaxIdx[setIdx]; i++) {
-        yield* helper(arrCounter.concat(i), setIdx);
-      }
-    }
-  }
-
-  yield* helper([], -1);
-}
-
 type CounterHighFirstOptions<T, R> = {
   sets: T[],
   getElemMaxIdx: (elem: T) => number,
